a59c96a0c215391dd2996b95c5bb148769b3ce9e
Ramandeep Kumar <69410508+kumardeepraman@users.noreply.github.com>
2026-03-30 15:13:01 +0200

    Create setup_steps.sh for installation process
    Added a setup script for installing the Epic environment and cloning necessary repositories.

---
+++ installingIRTv1/setup_steps.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+# ================================
+# === Functions for each step ===
+# ================================
+
+
+install_eic_shell() {
+    echo "This is Module A:"
+    echo
+    echo "=== Installing Epic environment ==="
+    curl --location https://get.epic-eic.org | bash
+
+
+    echo
+    echo "eic-shell installed."
+    echo
+    echo "Next step before Module B is:"
+    echo "   Enter the shell:   ./eic-shell"
+    echo
+
+}
+
+
+enter_drich_dev() {
+    echo "This is Module B:"
+    echo
+    echo "=== Cloning the drich-dev repository ===="
+    git clone https://github.com/eic/drich-dev.git 
+    
+    echo
+    echo "drich-dev cloned."
+    echo
+    echo "Next step before Module C is:"
+    echo "   Entered drich-dev/: cd drich-dev"
+    echo 
+}
+
+clone_repos() {
+    echo "This is Module C:"
+    echo
+    echo "=== Cloning drich-dev dependencies ==="
+    git clone https://github.com/eic/epic.git
+    git clone https://github.com/eic/EICrecon.git 
+    git clone --branch v1.0.10 --depth 1 https://github.com/eic/irt.git
+    git clone https://github.com/eic/EDM4eic.git
+    git clone https://eicweb.phy.anl.gov/EIC/benchmarks/detector_benchmarks.git
+    git clone https://github.com/kumardeepraman/Installing_Drich-dev.git
+
+    echo
+    echo "Dependencis cloned & files fixed fixed."
+    echo
+
+    cp Installing_Drich-dev/recon_sandbox.yaml config/recon_sandbox.yaml
+    cp Installing_Drich-dev/environ.sh .
+    cp Installing_Drich-dev/momentum_scan.rb scripts/momentum_scan.rb
+    cp Installing_Drich-dev/momentum_scan_2D_draw.C scripts/src/momentum_scan_2D_draw.C
+    cp Installing_Drich-dev/g4dRIChOptics.hh src/g4dRIChOptics.hh
+    cp Installing_Drich-dev/dconfig-chandra.C irt/delphes/scripts/dconfig-chandra.C
+    cp Installing_Drich-dev/dconfig.C irt/delphes/scripts/dconfig.C
+    cp Installing_Drich-dev/dloop.C irt/delphes/scripts/dloop.C
+    cp Installing_Drich-dev/IrtGeoDRICH.cc EICrecon/src/services/geometry/richgeo/IrtGeoDRICH.cc
+    cp Installing_Drich-dev/IrtGeoPFRICH.cc EICrecon/src/services/geometry/richgeo/IrtGeoPFRICH.cc
+    cp Installing_Drich-dev/CherenkovPIDAnalysis.cc detector_benchmarks/benchmarks/rich/src/CherenkovPIDAnalysis.cc
+    cp Installing_Drich-dev/CMakeLists.txt detector_benchmarks/CMakeLists.txt
+    cp Installing_Drich-dev/draw_benchmark.py detector_benchmarks/benchmarks/rich/draw_benchmark.py
+
+    echo "Next step before Module D is:"
+    echo "   Source the drich environment: source environ.sh"
+    echo
+
+}
+
+
+build_core() {
+    echo "This is Module D:"
+    echo
+    echo "=== Building core components ==="
+    ./build.sh epic
+    ./build.sh irt
+    ./build.sh EDM4eic
+    ./build.sh EICrecon
+    ./build.sh detector_benchmarks
+
+    echo
+    echo "Next step before Module E is:"
+    echo "   source environ.sh"
+}
+
+
+final_build() {
+    echo "This is Module E:"
+    echo
+    #echo "=== Final build ==="
+    #make
+    ln -s detector_benchmarks/benchmarks/rich/run_benchmark.rb benchmark2.rb
+    sed -i "222c outputName = re.sub(r'\\.root$', '', outputFileName)" simulate.py
+}
+
+# ================================
+# === Command-line argument parser
+# ================================
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 [--all] [--A] [--B] [--C]"
+    echo "          [--D] [--E]"
+    exit 1
+fi
+
+for arg in "$@"; do
+    case $arg in
+        --all)
+	    install_eic_shell
+            enter_drich_dev
+            clone_repos
+            build_core
+            final_build
+            ;;
+	--A)
+	    install_eic_shell
+	    ;;
+        --B)
+            enter_drich_dev
+            ;;
+        --C)
+            clone_repos
+            ;;
+        --D)
+            build_core
+            ;;
+        --E)
+            final_build
+            ;;
+        *)
+            echo "❌ Unknown option: $arg"
+            exit 1
+            ;;
+    esac
+done
+
+echo "Done. Remember to manually source environ.sh when needed."
